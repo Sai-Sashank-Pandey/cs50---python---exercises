a5b0723ea86683381c1f692427512eae00cc5517
Sai-Sashank-Pandey <sashanksai0777@gmail.com>
2026-02-25 06:04:38 +0000

    Wed, Feb 25, 2026, 11:34 AM +05:30

--- calculator.py
+++ calculator.py
@@ -1,3 +1,10 @@
 x=int(input("What's x? "))
+operator=input("+,-,*,/")
 y=int(input("What's y? "))
+match operator:
+    case '+':print("Result = ",x+y)
+    case '-':print("Result = ",x-y)
+    case '*':print("Result = ",x*y)
+    case '/':print("Result = ",x/y)
+    case '_':print("Result not found")
 
