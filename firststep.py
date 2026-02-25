d75ee7b8d86bdf29f7101cc5d989642ec6ab06b1
Sai-Sashank-Pandey <sashanksai0777@gmail.com>
2026-02-25 05:41:39 +0000

    Wed, Feb 25, 2026, 11:11 AM +05:30

--- firststep.py
+++ firststep.py
@@ -1,2 +1,22 @@
-name = input("What's your name? ")
+#code to ask the name
+name = input("What's your name? ").strip().title()
+#We could add string methods while declaring the string
+'''
+This is a multi line comment
+'''
 print("Hello",name,"!")
+
+#Code with using END parameter
+name = input("What's your name? ")
+print("Hello," ,end="")
+print(name)
+
+#Formatting Strings
+name=input("What's your name? ")
+'''This is another way of using string methods'''
+name=name.strip()
+name=name.title()
+'''split string function is used when you have to break a string into substrings'''
+first,last=name.split()
+print(f"Hello,{first}")
+#f prints the string literal directly
