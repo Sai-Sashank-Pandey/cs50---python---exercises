e45974afdcf3cbcdb3f3c94e482a3524d7025818
Sai-Sashank-Pandey <sashanksai0777@gmail.com>
2026-02-27 17:21:04 +0000

    Fri, Feb 27, 2026, 10:51 PM +05:30

--- functions.py
+++ functions.py
@@ -1,6 +1,8 @@
+def main():
+    hello()
+    name=input("What's your name? ")
+    hello(name)
+    hello("daniel day lewis")
 def hello(x='Pandey'):
     print("Hello,",x.title())
-hello()
-name=input("What's your name? ")
-hello(name)
-hello("daniel day lewis")
+main()
