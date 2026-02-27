edc274ede375270f4e9fc08dce813364cd914774
Sai-Sashank-Pandey <sashanksai0777@gmail.com>
2026-02-27 17:36:18 +0000

    Fri, Feb 27, 2026, 11:06 PM +05:30

--- functions2.py
+++ functions2.py
@@ -1,6 +1,6 @@
 def main():
     x=int(input("What's x? "))
-    print("square of x is ",square(x))
+    print(square(x))
 def square(to):
-    print(to*to)
+    print("square of x is ",to*to)
 main()
