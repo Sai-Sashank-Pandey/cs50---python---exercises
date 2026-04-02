d987f6955d012f40b85b38764d80d7aabcd28e55
Sai-Sashank-Pandey <sashanksai0777@gmail.com>
2026-04-02 13:26:38 +0000

    Thu, Apr 2, 2026, 6:56 PM +05:30

--- week1/conditionals/recommendations.py
+++ week1/conditionals/recommendations.py
@@ -1,15 +1,19 @@
-difficulty=input("Hard or Easy? ")
-players=input("Multiplayer or Single player? ")
-if difficulty == "Hard":
-    if players == "Multiplayer":
-        recommend("Call of Duty")
+def main():
+    difficulty=input("Hard or Easy? ")
+    players=input("Multiplayer or Single player? ")
+
+    if difficulty == "Hard":
+         if players == "Multiplayer":
+             recommend("Call of Duty")
+         else:
+            recommend("Resident Evil")
     else:
-        recommend("Resident Evil")
-else:
-    if players == "Multiplayer":
-        recommend("Clash of clans")
-    else:
-        recommend("Granny")
+        if players == "Multiplayer":
+            recommend("Clash of clans")
+        else:
+            recommend("Granny")
 
 def recommend(game):
-    print("We recommend you",game)
+    print("We recommend you,",game)
+
+main()
