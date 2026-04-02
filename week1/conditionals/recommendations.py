555f560df2261cb7c0934341a2e4fa1b01dc6c26
Sai-Sashank-Pandey <sashanksai0777@gmail.com>
2026-04-02 13:19:14 +0000

    Thu, Apr 2, 2026, 6:49 PM +05:30

--- week1/conditionals/recommendations.py
+++ week1/conditionals/recommendations.py
@@ -1,12 +1,12 @@
 difficulty=input("Hard or Easy? ")
 players=input("Multiplayer or Single player? ")
-if difficulty == Hard:
-    if players == Multiplayer:
+if difficulty == "Hard":
+    if players == "Multiplayer":
         print("Call of Duty")
     else:
         print("Resident Evil")
 else:
-    if players == Multiplayer:
+    if players == "Multiplayer":
         print("Clash of clans")
     else:
         print("Granny")
