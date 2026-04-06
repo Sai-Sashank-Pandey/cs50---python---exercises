3ee5b0a1fdae3292874b875b38967ea21c417371
Sai-Sashank-Pandey <sashanksai0777@gmail.com>
2026-04-06 16:45:51 +0000

    Mon, Apr 6, 2026, 10:15 PM +05:30

--- week1/problemset1/deep/deep.py
+++ week1/problemset1/deep/deep.py
@@ -1,5 +1,6 @@
 a = input("What is the answer to the Great Question of life, the Universe and Everything?  ").strip().lower()
-if a == "42" or a == "forty-two" or a == "forty two":
+#if a == "42" or a == "forty-two" or a == "forty two":
+if a in ["42","forty-two","forty two"]:
     print("Yes")
 else:
     print("No")
