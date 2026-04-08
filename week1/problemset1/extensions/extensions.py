2b5288b0b28994c714acb6c35c2a530e29e17de5
Sai-Sashank-Pandey <sashanksai0777@gmail.com>
2026-04-08 06:57:18 +0000

    Wed, Apr 8, 2026, 12:27 PM +05:30

--- week1/problemset1/extensions/extensions.py
+++ week1/problemset1/extensions/extensions.py
@@ -3,6 +3,15 @@ if name.endswith(".gif"):
     print("image/gif")
 elif name.endswith(".jpg"):
     print("image/jpeg")
-elif name.endswith("jpeg"):
+elif name.endswith(".jpeg"):
     print("image/jpeg")
-
+elif name.endswith(".png"):
+    print("image/png")
+elif name.endswith(".pdf"):
+    print("application/pdf")
+elif name.endswith(".txt"):
+    print("text/plain")
+elif name.endswith(".zip"):
+    print("application/zip")
+else:
+    print("application/octet-stream")
