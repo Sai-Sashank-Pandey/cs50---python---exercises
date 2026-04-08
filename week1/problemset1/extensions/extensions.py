54b30e8fff27389591c3b72291a18fa04b8615c9
Sai-Sashank-Pandey <sashanksai0777@gmail.com>
2026-04-08 06:50:33 +0000

    Wed, Apr 8, 2026, 12:20 PM +05:30

--- week1/problemset1/extensions/extensions.py
+++ week1/problemset1/extensions/extensions.py
@@ -2,7 +2,7 @@ name = input("File name: ")
 if name.endswith(".gif"):
     print("image/gif")
 elif name.endswith(".jpg"):
-    print("image/jpg")
+    print("image/jpeg")
 elif name.endswith("jpeg"):
     print("image/jpeg")
-    
+
