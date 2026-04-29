612530fc7d627c75855456061c2ea578f0d93071
Sai-Sashank-Pandey <sashanksai0777@gmail.com>
2026-04-29 05:10:03 +0000

    Wed, Apr 29, 2026, 10:40 AM +05:30

--- week1/problemset1/interpreter/interpreter.py
+++ week1/problemset1/interpreter/interpreter.py
@@ -1,8 +1,14 @@
-exp =input("Expression: ")
+exp =input("Expression: ").strip()
 x, y, z = exp.split()
 x = float(x)
-z = float(y)
+z = float(z)
 if y == "+":
     result = x + z
-    
+elif y == "-":
+    result = x - z
+elif y == "*":
+    result = x * z
+elif y == "/":
+    result = x / z
+print(f"{result:.1f}")
 
